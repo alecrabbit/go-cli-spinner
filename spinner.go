e5a9d832cfc6ae1246527d33d2b237b11dfa2174
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-23 21:06:26 +0300

    wp 11

--- spinner.go
+++ spinner.go
@@ -27,8 +27,8 @@ type ColorLevel int
 const (
     // NoColor no color support
     NoColor ColorLevel = iota
-    // Color represents 16 color level support
-    Color ColorLevel = 1 << (4 * iota)
+    // Color16 represents 16 color level support
+    Color16 ColorLevel = 1 << (4 * iota)
     // Color256 represents 256 color level support
     Color256
     // TrueColor represents true color level support
@@ -37,12 +37,16 @@ const (
 
 // Spinner struct representing spinner instance
 type Spinner struct {
-    Interval       time.Duration  // interval between spinner refreshes
-    frames         *ring.Ring     // frames holds chosen character set
-    colors         *ring.Ring     // colors holds chosen colorize set
-    active         bool           // active holds the state of the spinner
-    FinalMessage   string         // spinner final message, displayed after Stop()
-    currentMessage string         // string
+    Interval       time.Duration // interval between spinner refreshes
+    frames         *ring.Ring    // frames holds chosen character set
+    colors         *ring.Ring    // colors holds chosen colorize set
+    active         bool          // active holds the state of the spinner
+    FinalMessage   string        // spinner final message, displayed after Stop()
+    currentMessage string        // string
+    FormatMessage  string
+    FormatFrames   string
+    FormatProgress string
+    outputFormat   string
     progress       string         // string
     Reversed       bool           // flag, spin in the opposite direction
     colorLevel     ColorLevel     // current color level
@@ -72,6 +76,9 @@ func New(t int, d time.Duration) *Spinner {
         Writer:         os.Stderr,
         colorLevel:     Color256,
         FinalMessage:   "",
+        FormatMessage:  "%s ",
+        FormatFrames:   "%s ",
+        FormatProgress: "%s ",
         currentMessage: "",
         stop:           make(chan bool),
         HideCursor:     true,
@@ -87,14 +94,15 @@ func New(t int, d time.Duration) *Spinner {
         s.colors.Value = fmt.Sprintf("\x1b[38;5;%vm%s\x1b[0m", colors[i], "%s")
         s.colors = s.colors.Next()
     }
-
+    s.updateOutputFormat()
     // Override os specific settings
-    s = specificSettings(s)
+    s = platformOverrides(s)
 
     return &s
 }
 
-func specificSettings(s Spinner) Spinner {
+// Method to override settings
+func platformOverrides(s Spinner) Spinner {
     // if s.Writer == os.Stderr {
     // 	s.colorLevel = NoColor
     // }
@@ -111,28 +119,51 @@ func (s *Spinner) IsActive() bool {
 
 // Get current frame
 func (s *Spinner) getFrame() string {
-    // Note: external lock is needed
+    // Note: external lock
     // Rotate Ring
     if s.Reversed {
         s.frames = s.frames.Prev() // Backward
     } else {
         s.frames = s.frames.Next() // Forward
     }
-    return fmt.Sprintf("%s %s %s", s.colorize(s.frames.Value.(string)), s.currentMessage, s.progress)
+    frame := s.frames.Value.(string)
+    // format := s.createFormat(frame)
+    return fmt.Sprintf(s.outputFormat, s.colorize(frame), s.currentMessage, s.progress)
+    // return fmt.Sprintf(format, s.colorize(frame), s.currentMessage, s.progress)
+}
+
+func (s *Spinner) createFormat(frame string) string {
+    // Note: external lock
+    var format string
+    if frame != "" {
+        format += s.FormatFrames
+    }
+    if s.currentMessage != "" {
+        format += s.FormatMessage
+    }
+    if s.progress != "" {
+        format += s.FormatProgress
+    }
+    return format
 }
 
 // Colorize in string
 func (s *Spinner) colorize(in string) string {
-    // Rotate Ring
-    s.colors = s.colors.Next()
-    // Sprintf accordignly to colors format
-    return fmt.Sprintf(s.colors.Value.(string), in)
+    // Note: external lock
+    if s.colorLevel >= Color256 {
+        // Rotate Ring
+        s.colors = s.colors.Next()
+        // Sprintf accordignly to colors format
+        return fmt.Sprintf(s.colors.Value.(string), in)
+    }
+    return in
 }
 
 // Start will start the indicator
 func (s *Spinner) Start() {
     s.lock.Lock()
     if s.active {
+        // If already active - do nothing
         s.lock.Unlock()
         return
     }
@@ -159,12 +190,17 @@ func (s *Spinner) Start() {
     }()
 }
 
+// Get current frame and assign it to lastOutput
 func (s *Spinner) updateLastOutput() {
-    // Note: external lock is needed
+    // Note: external lock
     frame := s.getFrame()
     // Add move cursor back ansi sequence
-    frame += fmt.Sprintf("\x1b[%vD", runewidth.StringWidth(frame))
-    s.lastOutput = frame
+    s.lastOutput = frame + fmt.Sprintf("\x1b[%vD", s.frameWidth(frame))
+}
+
+func (s *Spinner) updateOutputFormat() {
+    // Note: external lock
+    s.outputFormat = fmt.Sprintf("%s%s%s", s.FormatFrames, s.FormatMessage, s.FormatProgress)
 }
 
 // Stop stops the spinner
@@ -198,14 +234,17 @@ func (s *Spinner) Erase() {
     s.lock.Unlock()
 }
 
+// Write erasing sequence to output
 func (s *Spinner) erase() {
-    // Note: external lock is needed
+    // Note: external lock
     if s.active {
-        _, err := fmt.Fprint(s.Writer, fmt.Sprintf("\x1b[%vX", runewidth.StringWidth(s.strip(s.lastOutput))))
-        if err != nil {
-            panic(err)
-        }
+        _, _ = fmt.Fprint(s.Writer, fmt.Sprintf("\x1b[%vX", s.frameWidth(s.lastOutput)))
     }
+}
+
+// Get frame width
+func (s *Spinner) frameWidth(f string) int {
+    return runewidth.StringWidth(s.strip(f))
 }
 
 // Last prints out last spinner output
@@ -216,7 +255,7 @@ func (s *Spinner) Last() {
 }
 
 func (s *Spinner) last() {
-    // Note: external lock is needed
+    // Note: external lock
     _, _ = fmt.Fprint(s.Writer, s.lastOutput)
 }
 
@@ -239,7 +278,7 @@ func (s *Spinner) Progress(p float32) {
         s.progress = r
     } else {
         s.progress = ""
+        // s.FormatProgress = ""
     }
     s.lock.Unlock()
 }
-
