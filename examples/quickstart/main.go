374ae669b9ce01cb2bef05d70afe13583faf5349
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-10-13 15:03:31 +0300

    example

--- examples/quickstart/main.go
+++ examples/quickstart/main.go
@@ -13,9 +13,20 @@ func main() {
 	s.Start()
 
 	// Doing some work
-	fmt.Println("Message One: is written to StdOut.")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Message Two: is written to StdOut.")
+	{
+		time.Sleep(1 * time.Second)
+		s.Progress(0.1) // -> 10%
+		fmt.Println("Message One: is written to StdOut.")
+		time.Sleep(1 * time.Second)
+		s.Progress(0) // Hide progress indicator
+		s.Message("Message") // Set message
+		time.Sleep(1 * time.Second)
+		fmt.Println("Message Two: is written to StdOut.")
+		s.Message("") // Hide message
+		time.Sleep(1 * time.Second)
+		s.Progress(1.0) // -> 100%
+		time.Sleep(500 * time.Millisecond)
+	}
 
 	// Stop spinner
 	s.Stop()
