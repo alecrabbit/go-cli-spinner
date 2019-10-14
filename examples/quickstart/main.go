2f65df9a3b1f046c63222f4eca1cb7797c6213ea
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-10-14 17:04:27 +0300

    example

--- examples/quickstart/main.go
+++ examples/quickstart/main.go
@@ -18,7 +18,7 @@ func main() {
 		s.Progress(0.1) // -> 10%
 		fmt.Println("Message One: is written to StdOut.")
 		time.Sleep(1 * time.Second)
-		s.Progress(0) // Hide progress indicator
+		s.Progress(0)        // Hide progress indicator
 		s.Message("Message") // Set message
 		time.Sleep(1 * time.Second)
 		fmt.Println("Message Two: is written to StdOut.")
