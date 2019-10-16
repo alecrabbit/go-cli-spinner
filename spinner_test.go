735cd77ef29bfc24259139b2e0f45dc60292b274
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-10-16 16:34:01 +0300

    coverage

--- spinner_test.go
+++ spinner_test.go
@@ -1,11 +1,10 @@
 package spinner
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
-	"github.com/mattn/go-runewidth"
+	"github.com/alecrabbit/go-cli-spinner/color"
 )
 
 // TestNew verifies that the returned instance is of the proper type
@@ -17,7 +16,15 @@ func TestNew(t *testing.T) {
 			CharSet(cs.chars),
 			HideCursor(true),
 			Reverse(),
-			)
+			ColorLevel(color.TNoColor),
+			Order(Char, Progress, Message),
+			ProgressFormat("%6s"),
+			ProgressIndicatorFormat("%.1f%%"),
+			MessageFormat("(%s)"),
+			Format("-%s -"),
+			Prefix("\x1b[38;5;161m>>\x1b[0m"),
+			FinalMessage("\x1b[38;5;34mDone!\x1b[0m\n"),
+		)
 		tp := reflect.TypeOf(s).String()
 		if tp != "*spinner.Spinner" {
 			t.Errorf("New returned incorrect type kind=%d %v", i, tp)
@@ -26,57 +33,39 @@ func TestNew(t *testing.T) {
 			t.Errorf("Expected new instance to be inactive (%d)", i)
 		}
 	}
-	// for i := 0; i < len(CharSets); i++ {
-	// 	s, _ := New(Variant(i))
-	// 	tp := reflect.TypeOf(s).String()
-	// 	if tp != "*spinner.Spinner" {
-	// 		t.Errorf("New returned incorrect type kind=%d %v", i, tp)
-	// 	}
-	// 	if s.Active() != false {
-	// 		t.Errorf("Expected new instance to be inactive (%d)", i)
-	// 	}
-	// }
 }
 
 /*
 Benchmarks
 */
-var result interface{}
-
-// BenchmarkNew runs a benchmark for the New() function
-func BenchmarkNew(b *testing.B) {
-	var s *Spinner
-	for n := 0; n < b.N; n++ {
-		s, _ = New()
-	}
-	result = s
-}
-
-// BenchmarkIfOne ...
-func BenchmarkIfOne(b *testing.B) {
-	var d int
-	for n := 0; n < b.N; n++ {
-		d = runewidth.StringWidth(" ") +
-			runewidth.StringWidth(fmt.Sprintf(" %s ", " "))
-	}
-	result = d
-	// fmt.Printf("One %s", result)
-}
-
-// BenchmarkIfTwo ...
-func BenchmarkIfTwo(b *testing.B) {
-	var d int
-	for n := 0; n < b.N; n++ {
-		d = runewidth.StringWidth(" " + fmt.Sprintf(" %s ", " "))
-	}
-	result = d
-	// fmt.Printf("Two %s", result)
-}
-
-// func BenchmarkNewStartStop(b *testing.B) {
-//    for n := 0; n < b.N; n++ {
-//        s := New(CharSets[1], 1*time.Second)
-//        s.Start()
-//        s.Stop()
-//    }
+// var result interface{}
+//
+// // BenchmarkNew runs a benchmark for the New() function
+// func BenchmarkNew(b *testing.B) {
+// 	var s *Spinner
+// 	for n := 0; n < b.N; n++ {
+// 		s, _ = New()
+// 	}
+// 	result = s
+// }
+//
+// // BenchmarkIfOne ...
+// func BenchmarkIfOne(b *testing.B) {
+// 	var d int
+// 	for n := 0; n < b.N; n++ {
+// 		d = runewidth.StringWidth(" ") +
+// 			runewidth.StringWidth(fmt.Sprintf(" %s ", " "))
+// 	}
+// 	result = d
+// 	// fmt.Printf("One %s", result)
+// }
+//
+// // BenchmarkIfTwo ...
+// func BenchmarkIfTwo(b *testing.B) {
+// 	var d int
+// 	for n := 0; n < b.N; n++ {
+// 		d = runewidth.StringWidth(" " + fmt.Sprintf(" %s ", " "))
+// 	}
+// 	result = d
+// 	// fmt.Printf("Two %s", result)
 // }
