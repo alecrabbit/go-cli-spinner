b719ab42550e07a72b7573798fb0553c9f202482
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-10-17 18:45:01 +0300

    tests

--- spinner_test.go
+++ spinner_test.go
@@ -35,6 +35,18 @@ func TestNew(t *testing.T) {
 	}
 }
 
+// TestSets verifies that set can be used
+func TestSets(t *testing.T) {
+	for idx, sp := range color.Prototypes {
+		r := sp.Handler(sp.ANSIStyles)
+		tp := reflect.TypeOf(r).String()
+		expected := "[]string"
+		if tp != expected {
+			t.Errorf("returned incorrect type for idx %v, expected: %v, given: %v", idx, expected, tp)
+		}
+	}
+}
+
 /*
 Benchmarks
 */
